09f48f305c9ef7712347a1b626e5bba59b344c9b
Luke Ghenco <lukeghenco@gmail.com>
2017-06-01 08:42:22 -0400

    Update starter code in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 //Code React element here
+
+
+
+ReactDOM.render(
+  {/* your element here */}, 
+  document.getElementById('root')
+);
