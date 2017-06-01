0a242fbea3167f7449042ec186797ad5635cfa69
Luke Ghenco <lukeghenco@gmail.com>
2017-06-01 08:56:25 -0400

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+const meInReact = 
+  React.createElement('div', { className: 'me' },
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+    React.createElement('ul', { className: 'my-interests' }, [
+      React.createElement('li', {}, 'JavaScript'),
+      React.createElement('li', {}, 'React'),
+      React.createElement('li', {}, 'Movies'),
+      React.createElement('li', {}, 'Ice cream')
+    ])
+  );
+
 ReactDOM.render(
   meInReact, 
   document.getElementById('root')
 );
 
 export default meInReact
-*/
