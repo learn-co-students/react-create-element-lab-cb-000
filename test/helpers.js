0cfb427d24cb7ff671f0083179f1fea2e10758f1
Luke Ghenco <lukeghenco@gmail.com>
2017-06-01 08:16:04 -0400

    Reconfigure helpers in test folder

--- test/helpers.js
+++ test/helpers.js
@@ -4,7 +4,7 @@ var jsdom = require('jsdom').jsdom;
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('');
+global.document = jsdom('<div id="root"></div>');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
@@ -15,4 +15,4 @@ Object.keys(document.defaultView).forEach((property) => {
 
 global.navigator = {
   userAgent: 'node.js'
-};  
+};
