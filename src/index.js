1f6e9298c83b765fd51d18762b26c4c22bb92f9f
Fer Hernandez <fh.1111@gmail.com>
2019-03-01 02:03:22 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,22 @@ ReactDOM.render(
 
 export default meInReact
 */
+
+
+const meInReact =
+  React.createElement('div', { className: 'me' },
+    React.createElement('h1', {}, 'An Awesome Person'),
+    React.createElement('p', {}, 'Who is learning React'),
+    React.createElement('ul', { className: 'my-interests' }, [
+      React.createElement('li', {}, 'JavaScript'),
+      React.createElement('li', {}, 'React'),
+      React.createElement('li', {}, 'Movies'),
+      React.createElement('li', {}, 'Ice cream')
+    ])
+  );
+
+
+
+
+
+export default meInReact
