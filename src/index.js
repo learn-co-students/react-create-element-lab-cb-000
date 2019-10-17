0c99c463d42855ff04d5be1c808ed403f0540daf
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-17 09:02:57 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -10,3 +10,22 @@ ReactDOM.render(
 
 export default meInReact
 */
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const description = React.createElement('p', {}, 'Who is learning React');
+const list = React.createElement('ul', {className:'my-interests'},
+      [
+        React.createElement('li', {}, 'JavaScript'),
+        React.createElement('li', {}, 'React'),
+        React.createElement('li', {}, 'Movies'),
+        React.createElement('li', {}, 'Ice cream')
+      ]
+    );
+
+const meInReact = React.createElement('div', { className: 'me' }, [title, description, list] )
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('global')
+);
+
+export default meInReact
